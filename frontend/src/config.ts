b4972586026bf6cfb4bdd9eb4bc4fcde34958545
lewis-king <lking1221@gmail.com>
2025-05-04 11:50:28 +0100

    Fix api path

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,4 +2,4 @@
 
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const API_BASE_URL = isLocalhost ? '/api' : import.meta.env.VITE_API_URL;
+export const API_BASE_URL = isLocalhost ? 'http://localhost:5001/api' : import.meta.env.VITE_API_URL + '/api';
